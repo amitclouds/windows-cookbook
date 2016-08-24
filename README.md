4044ac88eeaf902650ed2264e1c31932909bef17
Ubuntu <ubuntu@ip-172-31-61-86.ec2.internal>
2016-08-24 13:55:16 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# git-on-win
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'git-on-win'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures git-on-win'
+long_description 'Installs/Configures git-on-win'
+version '0.1.0'
